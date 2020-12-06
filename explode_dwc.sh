eb33ebdeddc2e05628e94809aeac08e45ab7b254
marimoracross <mmora@inbio.ac.cr>
2020-12-06 19:37:30 +0000

    Add mutimedia extension processing

--- explode_dwc.sh
+++ explode_dwc.sh
@@ -5,6 +5,9 @@
 # date          :20150709
 # usage         :bash explode_dwc.sh dwca_zip_file output_directory
 
+# Update:  
+#     Maria Mora: Add functionality to process multimedia.txt (20201205)
+
 function isRelative {
     local dir=$1
     if [ "${dir:0:1}" = "/" ]
@@ -30,13 +33,17 @@ fi
 working_dir="/tmp/"`date +%s%N`
 echo $working_dir
 
+#Unzip the DwC Archive in the /tmp
 mkdir $working_dir
 unzip -q $dwca -d $working_dir
 
-  # find datasetKey column index
+# find datasetKey column index
 dataset_column=`grep datasetKey $working_dir/meta.xml | grep -oP 'index="\K\d+'`
+
+# increase the value because index value starts in cero.
 dataset_column=$((dataset_column + 1))
 
+#Prepare directories and files to process each dataset
 for dataset in $(ls $working_dir/dataset)
 do
     dataset="${dataset/.xml/}"
@@ -55,9 +62,20 @@ do
 
 done
 
-# where the magic happens
+#Split occurrence.txt in a file for each datasetKey 
 awk -F '\t' -v dataset=$dataset_column -v output_dir=$output_dir 'NR>1 { print >> output_dir"/"$dataset"/occurrence.txt"}' $working_dir/occurrence.txt
 
+
+#Filter multimedia using the occurrence.txt (occurrence list) for each datasetKey 
+#for dataset in $(ls $working_dir/dataset)
+#do
+
+#  dataset="${dataset/.xml/}"
+#  echo "dataset: $dataset"
+#  awk -F '\t' 'NR==FNR{c[$1]++;next};c[$1]'  $output_dir/$dataset/occurrence.txt $working_dir/multimedia.txt > $output_dir/$dataset/multimedia.txt
+#done
+
+#Zip each DwC Archive, one for each datasetKey
 for dataset in $(ls $working_dir/dataset)
 do
    dataset="${dataset/.xml/}"
